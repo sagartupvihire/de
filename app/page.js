c247d713229ec10b0db7b6ba7ab0ba3eb5f4e658
sagar <sagartup20@gmail.com>
2024-01-17 14:31:14 +0530

    first commi1t

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 "use client"
 import { userAgent } from 'next/server'
-import React, { useState } from 'react'
+// pages/download.js
 
-const page = () => {
-  const [username, setUserName] = useState("")
+import React from 'react';
+
+const DownloadPage = () => {
+  const videoFileName = 'example.mp4';
+  const videoFilePath = `/videos/${videoFileName}`;
+
   return (
-    <>
-    <h1>enter your name{username}</h1>
-    <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="enter your name"/>
-    <h1>{username}</h1>
-    <button className='btn' onClick={()=>{setUserName("sagar")}}>update </button>
-    </>
-  )
-}
+    <div>
+      <h1>Video Download Page</h1>
+      <p>Click the link below to download the video:</p>
+      <a href={videoFilePath} download={videoFileName}>
+        Download Video
+      </a>
+    </div>
+  );
+};
 
-export default page
+export default DownloadPage;
